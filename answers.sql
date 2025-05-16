cb7e33f35ea448a2a39c00163a111234cf37dee4
Joey254 <joeymwangi7@gmail.com>
2025-05-16 11:51:42 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,5 @@
+-- Create a new database called salesDB with UTF-8 encoding
+CREATE DATABASE salesDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Drop (delete) the database called demo
+DROP DATABASE demo;
